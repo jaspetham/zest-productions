c1c543274af3747d8903dbe655b3e7256fe5b6c1
jaspetham <jaspertham98@gmail.com>
2022-12-02 19:07:25 +0800

    less buggy

--- components/Showcase.vue
+++ components/Showcase.vue
@@ -3,20 +3,26 @@
       <div class="scroll-thing-wrapper">
         <section class="scroll-thing hidden lg:block">
           <div id="row" class="row">
-            <h2 class="scroll-text">Showcase / Showcase / Showcase / Showcase / Showcase / Showcase / Showcase</h2>
+            <h2 class="scroll-text">Showcase / Showcase / Showcase / Showcase / Showcase / Showcase / Showcase </h2>
+            <h2 class="scroll-text">Showcase / Showcase / Showcase / Showcase / Showcase / Showcase / Showcase </h2>
+            <h2 class="scroll-text">Showcase / Showcase / Showcase / Showcase / Showcase / Showcase / Showcase </h2>
           </div>
           <div id="row-reverse" class="row">
             <h2 class="scroll-text">Jengs Homemade Brownies / BoBo Restaurant & Cafe / V Beauty Cosmetics / Cinnamonkins</h2>
+            <h2 class="scroll-text">Jengs Homemade Brownies / BoBo Restaurant & Cafe / V Beauty Cosmetics / Cinnamonkins</h2>
+            <h2 class="scroll-text">Jengs Homemade Brownies / BoBo Restaurant & Cafe / V Beauty Cosmetics / Cinnamonkins</h2>
           </div>
         </section>
-        <section class="lg:hidden scroll-thing">
+        <section class="lg:hidden scroll-thing mobile">
           <div class="marquee-container">
-            <div class="marquee" style="--play:running; --direction:normal; --duration:60s; --delay:0s; --iteration-count:infinite;">
+            <div class="marquee" style="--play:running; --direction:normal; --duration:30s; --delay:0s; --iteration-count:infinite;">
+              <h2 class="scroll-text">Showcase / Showcase / Showcase / Showcase / Showcase / Showcase / Showcase</h2>
               <h2 class="scroll-text">Showcase / Showcase / Showcase / Showcase / Showcase / Showcase / Showcase</h2>
             </div>
           </div>
           <div class="marquee-container">
-            <div class="marquee" style="--play:running; --direction:reverse; --duration:60; --delay:0s; --iteration-count:infinite;">
+            <div class="marquee" style="--play:running; --direction:reverse; --duration:30s; --delay:0s; --iteration-count:infinite;">
+              <h2 class="scroll-text">Jengs Homemade Brownies / BoBo Restaurant & Cafe / V Beauty Cosmetics / Cinnamonkins</h2>
               <h2 class="scroll-text">Jengs Homemade Brownies / BoBo Restaurant & Cafe / V Beauty Cosmetics / Cinnamonkins</h2>
             </div>
           </div>
@@ -46,23 +52,25 @@
           const lastChild = childrenArray[children - 1].getBoundingClientRect().right;
           const windowWidth = window.innerWidth;
 
-
-          if (lastChild < windowWidth) {
-              childrenArray.forEach((child) => {
-                row.appendChild(child.cloneNode(true));
-              });
-              childrenArrayReverse.forEach((child) => {
-                rowReverse.appendChild(child.cloneNode(true));
-              });
-          }
-
+          // if (lastChild < windowWidth) {
+          //     childrenArray.forEach((child) => {
+          //       // row.appendChild(child.cloneNode(true));
+          //     });
+          //     childrenArrayReverse.forEach((child) => {
+          //       // rowReverse.appendChild(child.cloneNode(true));
+          //     });
+          // }
           var elementTarget = document.querySelector(".scroll-thing-wrapper");
-          if (window.scrollY > (elementTarget.offsetTop - 120)) {
+          if (window.scrollY > (elementTarget.offsetTop - (headerHeight + 20))) {
             document.querySelector('.scroll-thing').classList.add('stick');
             document.querySelector('.scroll-thing').style.top = (headerHeight + 20) + 'px';
+            document.querySelector('.scroll-thing.mobile').classList.add('stick');
+            document.querySelector('.scroll-thing.mobile').style.top = (headerHeight + 20) + 'px';
           }else{
             document.querySelector('.scroll-thing').classList.remove('stick');
             document.querySelector('.scroll-thing').style.top = 0;
+            document.querySelector('.scroll-thing.mobile').classList.remove('stick');
+            document.querySelector('.scroll-thing.mobile').style.top = 0;
           }
         }});
     }
